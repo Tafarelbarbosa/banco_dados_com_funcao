d7e073264f2f8e09dee53a8cc7d56199357b45db
Tafarel Barbosa <tafarelhenrique@yahoo.com>
2024-11-07 22:20:10 -0300

    Criado e chamado a função Inserindo funcionarios na tabela

--- banco_com_funcao.py
+++ banco_com_funcao.py
@@ -9,20 +9,37 @@ def criar_banco(nome_banco):
 def criar_dados_tabela(conexao):
     cursor = conexao.cursor()
     cursor.execute('''
-        CREATE TABLE funcionarios2(
+        CREATE TABLE IF NOT EXISTS funcionarios2(
             id INTEGER PRIMARY KEY,
             nome TEXT NOT NULL,
             cargo TEXT NOT NULL,
             dataContratacao TEXT NOT NULL
         )
     ''')
-    conexao.commit
+    conexao.commit()
+    return cursor
 
-
+# INSERINDO FUNCIONARIOS NA TABELA
+def func_na_tabela():
+    nome = input('Digite um nome: ')
+    cargo = input('Digite um cargo: ')
+    data = input('Digite uma data no formato aaaa-mm-dd: ')
+    return nome,cargo,data
 
 
 # CRIAR UMA TABELA
 conexao = criar_banco('funcionarios2.db')
 
 # CRIAR DADOS DA TABELA
-criar_dados_tabela(conexao)
+cursor = criar_dados_tabela(conexao)
+
+# INSERINDO FUNCIONARIOS NA TABELA
+nome,cargo,data = func_na_tabela()
+cursor.execute("INSERT INTO funcionarios2 (id,nome,cargo,dataContratacao) VALUES (NULL,?,?,?)",(
+    nome,cargo,data))
+conexao.commit()
+
+nome,cargo,data = func_na_tabela()
+cursor.execute("INSERT INTO funcionarios2 (id,nome,cargo,dataContratacao) VALUES (NULL,?,?,?)",(
+    nome,cargo,data))
+conexao.commit()
