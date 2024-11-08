2703e55d366655627d3440b60d6ad9c77836fb93
Tafarel Barbosa <tafarelhenrique@yahoo.com>
2024-11-07 21:35:32 -0300

    Criado e chamado a função criar dados da tabela

--- banco_com_funcao.py
+++ banco_com_funcao.py
@@ -1,12 +1,28 @@
 import sqlite3
 
+#CRIAR TABELA
 def criar_banco(nome_banco):
     conexao = sqlite3.connect(nome_banco)
     return conexao
 
+# CRIAR DADOS DA TABELA
+def criar_dados_tabela(conexao):
+    cursor = conexao.cursor()
+    cursor.execute('''
+        CREATE TABLE funcionarios2(
+            id INTEGER PRIMARY KEY,
+            nome TEXT NOT NULL,
+            cargo TEXT NOT NULL,
+            dataContratacao TEXT NOT NULL
+        )
+    ''')
+    conexao.commit
 
 
-# Criar uma tabela
-criar_banco('funcionarios2.db')
 
 
+# CRIAR UMA TABELA
+conexao = criar_banco('funcionarios2.db')
+
+# CRIAR DADOS DA TABELA
+criar_dados_tabela(conexao)
